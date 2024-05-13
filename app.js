50453b12e79bd259e6f41592c430d8ec4bb5136d
yasminalkady <168604256+yasminalkady@users.noreply.github.com>
2024-05-13 13:47:47 +0300

    added mongoDB connection

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile('./views/home.html', { root: __dirname })
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+mongoose.connect('mongodb+srv://yasminhosny:AJRYNcb0reU08yiP@cluster0.thr7k5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
+  })})
+.catch((err)=>{console.log(err)})
